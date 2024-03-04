72f81b83711fb6d7021098d15195745954b2b32f
aidahrahmawati <aidahrahmawati04@gmail.com>
2024-03-04 11:36:45 +0700

    comit

--- main.js
+++ main.js
@@ -13,32 +13,34 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBNYqYJVYPTZRIK7pKZ4shbQ4igjseQpLM",
-  authDomain: "insan-cemerlang-d724d.firebaseapp.com",
-  projectId: "insan-cemerlang-d724d",
-  storageBucket: "insan-cemerlang-d724d.appspot.com",
-  messagingSenderId: "630693962922",
-  appId: "1:630693962922:web:a9447f760b858bcf781cd3"
+  apiKey: "AIzaSyBm9HdJ92vSLrKRclI6Z2J4bmvlFgR4AuU",
+  authDomain: "mang-yana.firebaseapp.com",
+  projectId: "mang-yana",
+  storageBucket: "mang-yana.appspot.com",
+  messagingSenderId: "1094982396668",
+  appId: "1:1094982396668:web:2d103526ab40a59efc0579",
+  measurementId: "G-MF48P7VG5P"
 };
 
+// Inisialisasi Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-export async function ambilDaftarPembeli() {
-  const refDokumen = collection(db, "pembeli");
-  const kueri = query(refDokumen, orderBy("nama pembeli"));
+export async function ambilDaftarProduk() {
+  const refDokumen = collection(db, "produk");
+  const kueri = query(refDokumen, orderBy("nama"));
   const cuplikanKueri = await getDocs(kueri);
 
   let hasil = [];
   cuplikanKueri.forEach((dok) => {
     hasil.push({
       id: dok.id,
-      namaPembeli: dok.data().namaPembeli,
-      alamat: dok.data(). alamat,
-      noTlpn: dok.data().noTlpn,
+      nama: dok.data().nama,
+      harga: dok.data().harga,
+      stok: dok.data().stok,
     });
   });
-
+  
   return hasil;
 }
 
@@ -46,16 +48,16 @@ export function formatAngka(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 }
 
-export async function  tambahPembeli(nama, alamat,tlpn) {
+export async function  tambahProduk(nama, harga, stok) {
   try {
-    const dokRef = await addDoc(collection(db, 'pembeli'),{
+    const dokRef = await addDoc(collection(db, 'produk'),{
       nama: nama,
-      alamat: alamat,
-      stok: noTlpn
+      harga: harga,
+      stok: stok
     });
-    console.log('Berhasil menambah Pembeli' + dok)
+    console.log('Berhasil menambah produk' + dokRef.id);
   } catch (e) {
-    Console.log('Gagal menambah Pembeli' + e);
+    Console.log('Gagal menambah produk' + e);
   }
 
 }
